63e16b3c6754430017929d8589c5731aa37f6cbe
Артюхов Александр <aaartyukhov@yandex.ru>
2023-03-02 17:35:05 +0300

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const { DB_ADDRESS } = require('./config');
+const routes = require('./routes');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+mongoose.connect(DB_ADDRESS, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// краш-тест сервера
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
