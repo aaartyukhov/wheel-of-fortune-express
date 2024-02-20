be806cf9e9fb18f6e0fbf37a8cb9e46c1afce344
Артюхов Александр <aaartyukhov@yandex.ru>
2024-02-20 16:05:33 +0300

    изменить локалхост

--- defaultEnv.js
+++ defaultEnv.js
@@ -1,5 +1,6 @@
 const PORT_DEFAULT = 3000;
-const DB_ADDRESS_DEFAULT = 'mongodb://localhost:27017/alfa-wheel';
+// const DB_ADDRESS_DEFAULT = 'mongodb://localhost:27017/alfa-wheel';
+const DB_ADDRESS_DEFAULT = 'mongodb://127.0.0.1:27017/alfa-wheel';
 
 module.exports = {
   PORT_DEFAULT,
